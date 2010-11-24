8a450f1d724e7ae50c9187d38478daa1584a3292
dykzei eleeot <dykzei@gmail.com>
2010-11-24 18:42:18 +1100

    initial

---
+++ src/dykzei/eleeot/GotHigh/Main.java
@@ -0,0 +1,13 @@
+package dykzei.eleeot.GotHigh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
